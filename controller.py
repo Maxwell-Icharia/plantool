3ef8f6bc8881bb3b6bae32767a53b220a429cd4c
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-25 17:18:25 +0300

    [Add] - Routes for redirecting

--- controller.py
+++ controller.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def root():
-    return render_template("dash.html")
+def home():
+    return render_template("home.html")
 
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
